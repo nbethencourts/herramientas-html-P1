e646d776808b7a4f3de420361a53223bfdaa09c6
Néstor Bethencourt <nbethencourts@uoc.edu>
2024-04-21 19:23:48 +0100

    se añade javascript para mostrar y ocultar letras de canciones

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,16 @@
 console.log('Prueba');
+const btnLetras = document.querySelectorAll('.btnLetra');
+
+btnLetras.forEach(btn => {
+    btn.addEventListener('click', function (event) {
+        event.preventDefault();
+    
+        if(event.target.nextElementSibling.hidden == true) {
+            event.target.nextElementSibling.hidden = false;
+            event.target.textContent = '-'
+        } else {
+            event.target.nextElementSibling.hidden = true;
+            event.target.textContent = '+'
+        }
+    });
+});
